1c4a13b3cb3f2c1b32744af6e6f45ccecafe2918
Sébastien Benat <sebastien.benat@gmail.com>
2017-11-20 18:19:26 -0500

    comments update

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
-## These functions are used to calculate and store in cache a matrix and its 
-## inverse matrix. So these data could be used without having to compute
+## These functions are used to calculate and store to cache a matrix and its 
+## inverse matrix. So these data could be used without having to compute again
 ## inverse matrix each time
 
-## the function makeCacheMatrix gets a matrix as input to store it in cache and
+## The function makeCacheMatrix gets a matrix as input to store it to cache and
 ## define access functions to the matrix and its inverse (get / set for matrix 
-## and inverse). Matrix and inverse are stored in cache by using <<- operator
-## each time makeCacheMatrix function is called, inverse matrix is deleted to
+## and inverse). Matrix and inverse are stored to cache by using <<- operator.
+## Each time makeCacheMatrix function is called, inverse matrix is deleted to
 ## to ensure consistency between matrix and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -26,10 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## the function cacheSolve takes the list creted by makeCacheMatrix function
+## The function cacheSolve takes the list creted by makeCacheMatrix function
 ## and return the inverse matrix of matrix used as arg in makeCacheMatrix. If 
 ## inverse Matrix has already been calculated, function use cache to get it, 
-## otherwise, function will recalculate it and use 'setinverse' function define  
+## otherwise function will recalculate it and use 'setinverse' function define  
 ## previously to store it in cache
 
 cacheSolve <- function(x, ...) {
